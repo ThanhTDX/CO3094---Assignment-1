47477a2c4c702ba976732c54616bb1eb43e897f4
John Nguyen <john12052003@gmail.com>
2023-11-22 00:07:31 +0700

    .

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ IP = "127.0.0.1"
 PORT = 4456
 SIZE = 1024
 FORMAT = "utf"
-SERVER_FOLDER = "server_folder"
 
 
 def handle_client_connection(conn, addr):
@@ -15,7 +14,7 @@ def handle_client_connection(conn, addr):
     folder_name = conn.recv(SIZE).decode(FORMAT)
 
     # Create the folder if it doesn't exist
-    folder_path = os.path.join(SERVER_FOLDER, folder_name)
+    folder_path = os.path.join("folder_in_server", folder_name)
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
         conn.send(f"Folder ({folder_name}) created.".encode(FORMAT))
@@ -27,31 +26,32 @@ def handle_client_connection(conn, addr):
         msg = conn.recv(SIZE).decode(FORMAT)
         cmd, data = msg.split(":")
 
-        if cmd == "FILENAME":
+        match cmd:
+            case "FILENAME":
             # Receive the file name
-            print(f"[CLIENT] Received the filename: {data}.")
+                print(f"[CLIENT] Received the filename: {data}.")
 
-            file_path = os.path.join(folder_path, data)
-            file = open(file_path, "w")
-            conn.send("Filename received.".encode(FORMAT))
+                file_path = os.path.join(folder_path, data)
+                file = open(file_path, "w")
+                conn.send("Filename received.".encode(FORMAT))
 
-        elif cmd == "DATA":
+            case "DATA":
             # Receive data from client
-            print(f"[CLIENT] Receiving the file data.")
-            file.write(data)
-            conn.send("File data received".encode(FORMAT))
+                print(f"[CLIENT] Receiving the file data.")
+                file.write(data)
+                conn.send("File data received".encode(FORMAT))
 
-        elif cmd == "FINISH":
+            case "FINISH":
             # Close the file and send confirmation
-            file.close()
-            print(f"[CLIENT] {data}.\n")
-            conn.send("The data is saved.".encode(FORMAT))
+                file.close()
+                print(f"[CLIENT] {data}.\n")
+                conn.send("The data is saved.".encode(FORMAT))
 
-        elif cmd == "CLOSE":
+            case "CLOSE":
             # Close the connection
-            conn.close()
-            print(f"[CLIENT] {data}")
-            break
+                conn.close()
+                print(f"[CLIENT] {data}")
+                break
 
 
 def main():
